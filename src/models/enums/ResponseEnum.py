101e300f0589bc82f1fc9766b667581e7cd21bdc
IbrahimAl-Labbad <ibrahimallabbad9@gmail.com>
2026-03-28 19:28:41 +0200

    feat: add models with ResponseEnum

---
+++ src/models/enums/ResponseEnum.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class ResponseSignal(Enum):
+    FILE_UPLOAD_SUCCESS = "File uploaded successfully"
+    FILE_TYPE_ERROR = "File type is not allowed"
+    FILE_SIZE_ERROR = "File size exceeds the maximum allowed limit"
+
+

fc4e73d778c21931acf2c8ce320d8e243cd78846
IbrahimAl-Labbad <ibrahimallabbad9@gmail.com>
2026-03-29 10:42:33 +0200

    feat: add ProcessingEnum to models

--- src/models/enums/ResponseEnum.py
+++ src/models/enums/ResponseEnum.py
@@ -4,5 +4,5 @@ class ResponseSignal(Enum):
     FILE_UPLOAD_SUCCESS = "File uploaded successfully"
     FILE_TYPE_ERROR = "File type is not allowed"
     FILE_SIZE_ERROR = "File size exceeds the maximum allowed limit"
-
-
+    PROCESSING_SUCCESS = "File processed successfully"
+    PROCESSING_FAILED = "Error processing the file"
